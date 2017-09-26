a68b9f520dcd07f9535d0163c724a0930b299dde
LuuucyXuuu <xu6@cooper.edu>
2017-09-25 22:59:54 -0400

    Completed Assignment

--- calc.c
+++ calc.c
@@ -1,2 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
 
+int main(int argc, char* argv[])
+{
+	for (int i = 1; i < argc; ++i)
+	{
+		char c = argv[i][0];
+		switch(c) 
+		{
+			case('+'):
+				printf("%f", atof(argv[i+1]) + atof(argv[i+2]));
+				break;
+			case('-'):
+				printf("%f", atof(argv[i+1]) - atof(argv[i+2]));
+				break;
+			case('x'):
+				printf("%f", atof(argv[i+1]) * atof(argv[i+2]));
+				break;
+			case('/'):
+				printf("%f", atof(argv[i+1]) / atof(argv[i+2]));
+				break;
+		}
+	}
+}
 
